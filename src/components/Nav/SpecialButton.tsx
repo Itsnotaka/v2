7446ca4ddc8959e0a6c6a75ee7fa23cf84b6894c
Daniel <itsnotaka@kakaka.dev>
2022-03-31 17:34:11 -0400

    Added shadow

--- src/components/Nav/SpecialButton.tsx
+++ src/components/Nav/SpecialButton.tsx
@@ -10,7 +10,7 @@ export default function SpecialButton({
 	return (
 		<a href={href} target="_blank" rel="noreferrer">
 			<Tooltip title={title}>
-				<button className=" flex h-12 w-12 shrink-0 transform-none cursor-pointer select-none items-center justify-center rounded-[23%] bg-primary-300 text-center text-black dark:bg-primary-500 dark:bg-opacity-50 dark:text-primary-400">
+				<button className="flex h-12 w-12 shrink-0 transform-none cursor-pointer select-none items-center justify-center rounded-[23%] bg-primary-300 text-center text-black shadow-lg dark:bg-primary-500 dark:bg-opacity-50 dark:text-primary-400">
 					{/* eslint-disable-next-line @next/next/no-img-element */}
 					<img src={'/static/bg.png'} alt="" className="h-6 w-6 rounded-full" />
 				</button>
