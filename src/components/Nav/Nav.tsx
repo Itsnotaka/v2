c7f5d38b60775da0473a77651d8be7c1643949d3
Daniel <itsnotaka@kakaka.dev>
2022-03-31 18:33:41 -0400

    Navbar color change

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function Nav({ children }: { children?: ReactNode }) {
 	return (
-		<footer className="fixed inset-x-0 bottom-0 left-1/2 flex h-[70px] w-[calc(100%_-_64px)] max-w-max -translate-x-1/2 -translate-y-1/2 justify-between rounded-[23px] border border-primary-200 bg-primary-100 px-[10px]  text-sm shadow-primary backdrop-blur dark:border-primary-400 dark:border-opacity-20 dark:bg-black dark:bg-opacity-[0.08]">
+		<footer className="fixed inset-x-0 bottom-0 left-1/2 flex h-[70px] w-[calc(100%_-_64px)] max-w-max -translate-x-1/2 -translate-y-1/2 justify-between rounded-[23px] border border-primary-200 bg-primary-100 px-[10px]  text-sm shadow-primary backdrop-blur dark:border-primary-400 dark:border-opacity-20 dark:bg-mirage-900 dark:bg-opacity-[0.08]">
 			<div className="flex w-full shrink-0 items-center gap-x-2 overflow-x-auto text-center">
 				{children}
 			</div>
