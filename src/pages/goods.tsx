3c68453b4f1409b903d831e27ad625c0ba72b18b
Daniel <itsnotaka@kakaka.dev>
2022-04-01 03:50:51 -0400

    Added a new page

---
+++ src/pages/goods.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Container from '../components/Container';
+
+const Goods: NextPage = () => (
+	<Container>
+		<div>hi</div>
+	</Container>
+);
+
+export default Goods;
