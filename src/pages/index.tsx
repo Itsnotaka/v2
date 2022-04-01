d48d5790c84caa9b47a61f42e25fe1c221368bf6
Daniel <itsnotaka@kakaka.dev>
2022-04-01 18:38:41 -0400

    Started the first page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,46 @@
+import React, { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import Container from '../components/Container';
+import Typist from 'react-typist';
 
-const Home: NextPage = () => (
-	<>
-		<title>Daniel Fu</title>
-		<Container>
-			<article className="prose dark:text-primary-250 prose-h1:dark:text-primary-250 prose-h1:text-2xl">
-				<h1>Min Chun Fu</h1>
-				<p>
-					For years parents have espoused the health benefits of eating garlic
-					bread with cheese to their children, with the food earning such an
-					iconic status in our culture that kids will often dress up as warm,
-					cheesy loaf for Halloween.
-				</p>
-				<p>
-					But a recent study shows that the celebrated appetizer may be linked
-					to a series of rabies cases springing up around the country.
-				</p>
-				{/* <!-- ... --> */}
-			</article>
-		</Container>
-	</>
-);
+const Home: NextPage = () => {
+	const texts = ['student', 'developer', 'designer'];
+	const [currentTextCounter, setCurrentTextCounter] = useState(0);
+
+	useEffect(() => {
+		// creating a timer to rotate the text every 3 seconds
+		const timer = setInterval(() => {
+			setCurrentTextCounter((currentTextCounter + 1) % texts.length);
+		}, 5000);
+
+		return () => clearInterval(timer);
+	}, [currentTextCounter, texts.length]);
+
+	return (
+		<>
+			<title>Daniel Fu</title>
+			<Container>
+				<article className="prose prose-h1:text-2xl dark:text-primary-250 prose-h1:dark:text-primary-250">
+					<h1 className="font-[monolisa]">Min Chun Fu</h1>
+					<p className="flex whitespace-pre-wrap align-bottom">
+						— A{' '}
+						<Typist
+							className={`${
+								texts[currentTextCounter] === 'developer'
+									? 'font-[monolisa]'
+									: ''
+							}`}
+							key={currentTextCounter}
+						>
+							{texts[currentTextCounter]}
+						</Typist>{' '}
+						from Taiwan.
+					</p>
+					<hr className="mt-3 h-[1px] w-12" />
+				</article>
+			</Container>
+		</>
+	);
+};
 
 export default Home;
