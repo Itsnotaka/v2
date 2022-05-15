3d3076f71caeb9abccd4c677a06349077d01563a
itsnotaka <itsnotaka@gmail.com>
2022-05-15 21:39:40 +0800

    FIxed Document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,6 +40,10 @@ class MyDocument extends Document {
 						content=" A developer/student/designer from Taiwan."
 					/>
 					{/* <meta property="og:image" content="https://rauno.me/og.png" /> */}
+					<link
+						rel="icon"
+						href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💫</text></svg>"
+					/>💫
 					<body>
 						<Main />
 						<NextScript />
