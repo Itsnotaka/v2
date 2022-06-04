42a0943d9d9cd8a1e4b9afe1efb423f0f4c12c69
itsnotaka <itsnotaka@gmail.com>
2022-06-04 11:56:55 +0800

    commented unused code

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -4,20 +4,20 @@ import Layout from '../Layout';
 import { Nav } from '../Nav';
 
 export default function Container({ children }: { children: ReactNode }) {
-	const [isOpen, setIsOpen] = useState(false);
+	// const [isOpen, setIsOpen] = useState(false);
 
-	useEffect(() => {
-		function onKeyDown(event: { key: string; metaKey: any; ctrlKey: any }) {
-			if (event.key === 'k' && (event.metaKey || event.ctrlKey)) {
-				setIsOpen(!isOpen);
-			}
-		}
+	// useEffect(() => {
+	// 	function onKeyDown(event: { key: string; metaKey: any; ctrlKey: any }) {
+	// 		if (event.key === 'k' && (event.metaKey || event.ctrlKey)) {
+	// 			setIsOpen(!isOpen);
+	// 		}
+	// 	}
 
-		window.addEventListener('keydown', onKeyDown);
-		return () => {
-			window.removeEventListener('keydown', onKeyDown);
-		};
-	}, [isOpen]);
+	// 	window.addEventListener('keydown', onKeyDown);
+	// 	return () => {
+	// 		window.removeEventListener('keydown', onKeyDown);
+	// 	};
+	// }, [isOpen]);
 
 	return (
 		<>
