ae2e42448dd4eba0213346acc867e4f34d798259
itsnotaka <itsnotaka@gmail.com>
2022-06-12 20:15:12 +0800

    Added z-index for always infront

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -15,7 +15,7 @@ import ThemeChanger from './ThemeChanger';
 
 export default function Nav() {
 	return (
-		<footer className="fixed inset-x-0 bottom-0 left-1/2 flex h-[70px] w-[calc(100%_-_64px)] max-w-max -translate-x-1/2 -translate-y-1/2 justify-between rounded-[23px] border border-primary-200 bg-primary-100 px-[10px]  text-sm shadow-primary backdrop-blur dark:border-primary-400 dark:border-opacity-20 dark:bg-mirage-900 dark:bg-opacity-[0.08]">
+		<footer className="z-10 fixed inset-x-0 bottom-0 left-1/2 flex h-[70px] w-[calc(100%_-_64px)] max-w-max -translate-x-1/2 -translate-y-1/2 justify-between rounded-[23px] border border-primary-200 bg-primary-100 px-[10px]  text-sm shadow-primary backdrop-blur dark:border-primary-400 dark:border-opacity-20 dark:bg-mirage-900 dark:bg-opacity-[0.08]">
 			<div className="flex w-full shrink-0 items-center gap-x-2 overflow-x-auto text-center">
 				{/* <SpecialButton href={''} title={'Flow'} /> */}
 				{/* <Separator /> */}
